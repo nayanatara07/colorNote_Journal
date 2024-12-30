2f611b6edb40945963f3d58929918a94665ead9c
Nayanatara <nayana2017tara@gmail.com>
2024-12-30 15:14:21 +0530

    remove unused styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from "react";
 import Journal from "./components/Journal";
 
 const App = () => {
-  return (
-    <div className="container-right">
-      <Journal />
-    </div>
-  );
+  return <Journal />;
 };
 
 export default App;
